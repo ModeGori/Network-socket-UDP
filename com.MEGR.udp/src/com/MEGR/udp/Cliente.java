d16d1dd68ee4f147cfd5d9502d51adc0e7a9f1e6
Unknown <moitix30@gmail.com>
2018-07-31 19:00:34 -0400

    Clases listas

--- com.MEGR.udp/src/com/MEGR/udp/Cliente.java
+++ com.MEGR.udp/src/com/MEGR/udp/Cliente.java
@@ -1,5 +1,84 @@
 package com.MEGR.udp;
-
+import java.io.*;
+import java.net.*;
 public class Cliente {
-
-}
+	
+		   public static void main(String argv[]) {
+		      if (argv.length == 0) {
+		         System.err.println("Java ClienteUDP Servidor");
+		         System.exit(1);
+		      }
+		 
+		      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		 
+		      DatagramSocket socket;
+		      InetAddress address;
+		      byte[] mensaje_bytes = new byte[256];
+		      byte[] MensajeServidor_bytes = new byte[256];
+		      String mensaje="";
+		      String mensajeServidor="";
+		      //Paquete para enviar al servidor
+		      DatagramPacket paquete;
+		      //Creamos el objeto paquete2 de tipo DatagramPacket para recibir del servidor
+		      DatagramPacket paquete2;
+		      mensaje_bytes=mensaje.getBytes();
+		 
+		      try {
+		 
+		    	  //Creamos el socket
+		    	  socket = new DatagramSocket();
+		 
+		         // Leemos el primer parametro, donde debe ir la direccion  
+		         // IP del servidor 
+		         address=InetAddress.getByName(argv[0]);
+		 
+		         mensaje = "Holaaaaaaa uwu";
+		         mensaje_bytes = mensaje.getBytes();
+		         //Creamos paquete
+		         paquete = new DatagramPacket(mensaje_bytes,mensaje.length(),address,6000);
+		         //Mandamos el paquete
+		         socket.send(paquete);
+		 
+		         //El mensaje recibido vendra en bytes
+		         MensajeServidor_bytes = new byte[256];
+		         //Esperamos a recibir un paquete
+		         paquete2 = new DatagramPacket(MensajeServidor_bytes,256);
+		         socket.receive(paquete2);
+		         //Convertimos el mensaje recibido en un string
+		 		 mensajeServidor = new String(MensajeServidor_bytes).trim();
+		 		 //Imprimimos el paquete recibido
+		 		 System.out.println(mensajeServidor);  
+		 
+		         do {
+		 
+		        	//Lee los caracteres introducidos por pantalla 
+		            mensaje = in.readLine();
+		            mensaje_bytes = mensaje.getBytes();
+		 
+		            //Creamos paquete
+		            paquete = new DatagramPacket(mensaje_bytes,mensaje.length(),address,6000);
+		 
+		            //Mandamos el paquete
+		            socket.send(paquete);
+		 
+		            //El mensaje recibido vendra en bytes
+		            MensajeServidor_bytes = new byte[256];
+		 
+		            //Esperamos a recibir un paquete
+		            paquete2 = new DatagramPacket(MensajeServidor_bytes,256);
+		            socket.receive(paquete2);
+		 
+		            //Convertimos el mensaje recibido en un string
+		    		mensajeServidor = new String(MensajeServidor_bytes).trim();
+		 
+		    		//Imprimimos el paquete recibido
+		    		System.out.println(mensajeServidor);  
+		 
+		         } while (!mensaje.startsWith("Fin"));
+		      }
+		      catch (Exception e) {
+		         System.err.println(e.getMessage());
+		         System.exit(1);
+		      }
+		   }
+		}
--- com.MEGR.udp/src/com/MEGR/udp/Server.java
+++ com.MEGR.udp/src/com/MEGR/udp/Server.java
@@ -1,5 +1,126 @@
 package com.MEGR.udp;
-
+import java.io.*;
+import java.net.*;
 public class Server {
-
-}
+	  public static void main(String argv[]) {
+		  
+	      DatagramSocket socket;
+	      boolean fin = false;
+	      //Creamos un objeto de tipo DatagramPacket para recibir un paquete del cliente
+	      DatagramPacket paquete;
+	      //Creamos paquete de vuelta para el cliente
+	      DatagramPacket paquete2;
+	      //Declaramos una variable de tipo string que guarda el mensaje recibido por el cliente
+	      String mensaje ="";
+	      //Declaramos una variable string para la cadena que le mandaremos al cliente
+	      String MensajeSalida ="";
+	 
+	      try {
+	 
+	    	 //Abrimos un socket en el puerto 6000
+	    	  //A traves de este socket enviaremos paquetes de tipo Datagram
+	         socket = new DatagramSocket(6000);
+	 
+	         //Nos preparamos para recibir una mensaje de 256 bytes
+	    	 byte[] mensaje_bytes = new byte[256];
+	    	 mensaje_bytes = new byte[256];
+	         //Creamos un contenedor de datagrama, el buffer sera el array mensaje_bytes
+	    	 paquete = new DatagramPacket(mensaje_bytes,256);
+	 
+	         do {
+	 
+	            mensaje_bytes = new byte[256];
+	 
+	            //Creamos un contenedor de datagram, el buffer sera el array mensaje_bytes
+	            paquete = new DatagramPacket(mensaje_bytes,256);
+	 
+	            //Esperamos a recibir un paquete
+	            socket.receive(paquete);
+	 
+	           //Convertimos el mensaje recibido en un string
+	        	mensaje = new String(mensaje_bytes).trim();
+	 
+	           //Imprimimos el paquete recibido
+	           System.out.println(mensaje);  
+	 
+	 
+	           //Condiciones encadenadas para mandar la información
+	           //dependiendo del dato recibido
+	           if (mensaje.startsWith("Holaaaaaaa uwu")) {
+	 
+	          	   MensajeSalida = "Holaaaaaaa uwu";
+	           }
+	           else if (mensaje.startsWith("ALL")) {
+	 
+	        	   MensajeSalida = "ALL :D:250; :V:300; :):540; :(:380; :/:240; :*:210; 3:):0; 0:):0; :P:300";
+	           	System.out.println(MensajeSalida);
+	 
+	           }
+	           else if(mensaje.startsWith(":D"))
+	           {
+	        	   MensajeSalida = ":D 250";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith(":V")) {
+	        	   MensajeSalida = ":V 300";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith(":)")) {
+	        	   MensajeSalida= ":) 540";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith(":(")) {
+	        	   MensajeSalida= ":( 380";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith(":/")) {
+	        	   MensajeSalida= ":/ 240";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith(":*")) {
+	        	   MensajeSalida= ":* 210";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith("3:)")) {
+	        	   MensajeSalida= "3:) 0";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith("0:)")) {
+	        	   MensajeSalida= "0:) 0";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith(":P")) {
+	        	   MensajeSalida= ":P 300";
+	           	System.out.println(MensajeSalida);
+	           }
+	           else if (mensaje.startsWith("Fin")) {
+	        	   MensajeSalida= "Fin";
+	       	    System.out.println(MensajeSalida);
+	       	    fin=true;
+	           }
+	           else{
+	        	   MensajeSalida = "ERROR";	
+	           	   System.out.println(MensajeSalida);
+	           }           
+	 
+	           //Nº de puerto desde donde se envio  
+	           int puerto = paquete.getPort();  
+	           //Dirección de Internet desde donde se envio
+	           InetAddress address = paquete.getAddress();
+	           //Preparamos el formato que le vamos a mandar
+	           byte[] mensaje2_bytes = new byte[256];
+	           //Guardamos en mensaje2_bytes el mensaje de salida formateado
+	           mensaje2_bytes = MensajeSalida.getBytes();
+	           //Generamos el paquete de vuelta, usando los datos del remitente del paquete original 
+	           paquete2 = new DatagramPacket(mensaje2_bytes,MensajeSalida.length(),address,puerto);
+	           // Enviamos  
+	           socket.send(paquete2);
+	 
+	         } while (!fin);
+	      }
+	      catch (Exception e) {
+	         System.err.println(e.getMessage());
+	         System.exit(1);
+	      }
+	   }
+	}
